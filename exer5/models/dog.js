2f0868949c2ccdbc36b7e624494d5f2388efab86
Grace Kim <gracekim3@gatech.edu>
2023-03-02 16:37:01 -0500

    add exer5 solutions

--- exer5/models/dog.js
+++ exer5/models/dog.js
@@ -1,8 +1,23 @@
 import mongoose from "mongoose"
 
 const dogSchema = new mongoose.Schema({
-
-
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    breed: {
+        type: String,
+        required: true
+    },
+    dateEnter: {
+        type: Date,
+        required: true
+    },
+    dateAdopt: Date
 })
 
 export default mongoose.model("Dog", dogSchema)
